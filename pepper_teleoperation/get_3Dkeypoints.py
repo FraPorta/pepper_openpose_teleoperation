0f71c0b45aefd9648996186645e9692b2e42336c
Francesco <francy857@gmail.com>
2021-04-27 15:10:08 +0200

    Added visualization of live 3D keypoints on a graph

--- pepper_teleoperation/get_3Dkeypoints.py
+++ pepper_teleoperation/get_3Dkeypoints.py
@@ -1,15 +1,58 @@
 import sys
-from socket_receive import SocketReceive 
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from socket_receive import SocketReceive
+
+import numpy as np
 
 try:
+    # Init dictionary
     wp_dict = {}
+
     # initialize socket for receiving the 3D keypoints
     sr = SocketReceive()
 
+    # Create matplotlib figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # to run GUI event loop
+    plt.ion()
+    
+        
+
     print("Start receiving keypoints...")
     while True:
+        # Receive keypoints from socket
         wp_dict = sr.receive_keypoints()
-        print(wp_dict)
+        # print(wp_dict)
+        if bool(wp_dict):
+            # Extract keypoints from dictionary
+            wp_list = list(wp_dict.values())
+            wp_arr = np.array(wp_list)
+            xs = wp_arr[:,0]
+            zs = wp_arr[:,1]
+            ys = wp_arr[:,2]
+            c = 'r'
+            m = 'o'
+            # Clear current figure 
+            plt.cla()
+            # Plot 3D keypoints
+            ax.scatter(xs, ys, zs, c=c, marker=m)
+            # Set labels
+            ax.set_xlabel('X Label')
+            ax.set_ylabel('Z Label')
+            ax.set_zlabel('Y Label')
+            # Set axes limits
+            ax.set_xlim(-1,1)
+            ax.set_ylim(0,2)
+            ax.set_zlim(-1,1)
+            # Update 3D plot
+            plt.show()
+            fig.canvas.draw()
+            fig.canvas.flush_events()
 
 except Exception as e:
     print(e)
