7535bed61eb568708bf03654c93a7d57820af0d7
Francesco Porta <francy857@gmail.com>
2021-07-15 16:52:10 +0200

    Now screen recorder saves the detected markers
    and shows the user when a new one is found highliting it with a different color for some time

--- openpose_wrap/screen_recorder.py
+++ openpose_wrap/screen_recorder.py
@@ -1,6 +1,7 @@
 # importing the required packages
 import pyautogui
 import cv2
+import os
 import numpy as np
 from datetime import datetime
   
@@ -10,18 +11,24 @@ resolution = (760, 435)
 # Specify video codec
 codec = cv2.VideoWriter_fourcc(*"XVID")
   
-# Specify name of Output file
+# create folder to store data
+# Create folder to save keypoints if it does not exist 
 now = datetime.now()
-# dd/mm/YY H:M:S
-dt_string = now.strftime("%d_%m_%Y_%H-%M-%S")
-filename = dt_string +"_rec.avi"
+dt_string = now.strftime("%d_%m_%Y_%H-%M-%S")      
+if not os.path.exists("video_data"):
+    os.mkdir("video_data")
+folder = "video_data/" + dt_string + "/"
+os.mkdir(folder)
+
+# Specify name of Output file
+videoname = folder + "arm_rec.avi"
   
 # Specify frames rate. We can choose any 
 # value and experiment with it
 fps = 15.0
   
 # Creating a VideoWriter object
-# out = cv2.VideoWriter(filename, codec, fps, resolution)
+out = cv2.VideoWriter(videoname, codec, fps, resolution)
   
 # Create an Empty window
 cv2.namedWindow("Pepper Arm Camera")
@@ -31,8 +38,20 @@ cv2.namedWindow("Pepper Arm Camera")
 
 arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_100)
 arucoParams = cv2.aruco.DetectorParameters_create()
-    
+
+# list to store the already found ids
+found_ids = []
+
+# dict to store the single marker info when found for the first time
+marker_info = {}
+# list to store the already found markers info
+markers_list = []
+
+highlight = {}
+count = {}
+
 while True:
+
     # Take screenshot using PyAutoGUI
     img = pyautogui.screenshot(region=(100, 20, 760, 435))
 
@@ -51,6 +70,7 @@ while True:
     if len(corners) > 0:
 		# flatten the ArUco IDs list
         ids = ids.flatten()
+        
 		# loop over the detected ArUCo corners
         for (markerCorner, markerID) in zip(corners, ids):
             # extract the marker corners (which are always returned
@@ -58,30 +78,69 @@ while True:
             # order)
             corners = markerCorner.reshape((4, 2))
             (topLeft, topRight, bottomRight, bottomLeft) = corners
+            
             # convert each of the (x, y)-coordinate pairs to integers
             topRight = (int(topRight[0]), int(topRight[1]))
             bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
             bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
             topLeft = (int(topLeft[0]), int(topLeft[1]))
 
-            # draw the bounding box of the ArUCo detection
-            cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
-            cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
-            cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
-            cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+            # Update the list of already found marker IDs
+            if not markerID in found_ids:
+                found_ids.append(markerID)
+                corn = [topRight, bottomRight, bottomLeft, topLeft]
+                timestamp = datetime.now().strftime('%H:%M:%S.%f')
+                
+                marker_info['id']           = markerID
+                marker_info['timestamp']    = timestamp
+                marker_info['topRight']     = topRight
+                marker_info['bottomRight']  = bottomRight
+                marker_info['bottomLeft']   = bottomLeft
+                marker_info['topLeft']      = topLeft
+                
+                markers_list.append(marker_info)
+                marker_info = {} 
+                
+                highlight[markerID] = True
+                count[markerID] = 0
+            
+            # if highlight:
+            #     highlight_markers = [k for k,v in highlight.items() if v == True]
+            
+            if count[markerID] > 10:
+                highlight[markerID] = False
+                count[markerID] = 0
+                
+            if highlight[markerID]: 
+                c = count[markerID]
+                count[markerID] = c + 1  
+                
+                # draw the bounding box of the ArUCo detection
+                cv2.line(frame, topLeft, topRight, (0, 255, 0), 4)
+                cv2.line(frame, topRight, bottomRight, (0, 255, 0), 4)
+                cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 4)
+                cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 4)
+            else:    
+                # draw the bounding box of the ArUCo detection
+                cv2.line(frame, topLeft, topRight, (255, 0, 0), 2)
+                cv2.line(frame, topRight, bottomRight, (255, 0, 0), 2)
+                cv2.line(frame, bottomRight, bottomLeft, (255, 0, 0), 2)
+                cv2.line(frame, bottomLeft, topLeft, (255, 0, 0), 2)
+            
             # compute and draw the center (x, y)-coordinates of the
             # ArUco marker
             cX = int((topLeft[0] + bottomRight[0]) / 2.0)
             cY = int((topLeft[1] + bottomRight[1]) / 2.0)
             cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+            
             # draw the ArUco marker ID on the frame
             cv2.putText(frame, str(markerID),
                         (topLeft[0], topLeft[1] - 15),
                         cv2.FONT_HERSHEY_SIMPLEX,
                         0.5, (0, 255, 0), 2)
-    
+
     # Write it to the output file
-    # out.write(frame)
+    out.write(frame)
       
     # Optional: Display the recording screen
     cv2.imshow('Pepper Arm Camera', frame)
@@ -89,9 +148,19 @@ while True:
     # Stop recording when we press 'q'
     if cv2.waitKey(1) == ord('q'):
         break
-  
+
 # Release the Video writer
-# out.release()
+out.release()
   
 # Destroy all windows
 cv2.destroyAllWindows()
+
+# Save data in a file
+with open(folder + "markers.txt", "w") as textfile:
+    textfile.write("Found markers:\n")
+    # If markers were found save info in a file
+    if markers_list:  
+        for line in markers_list:
+            textfile.write(str(line) + "\n")
+    textfile.close()
+  
